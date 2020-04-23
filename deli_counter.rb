35931c5c2125e41c418be57b9fa970ada87347ff
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-23 15:07:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,8 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    katz_deli.collect do |person|
-       "#{katz_deli.index(person)+1}. " << person
-    end
-    puts "The line is currently: " << katz_deli.join(",")
+    current_line = katz_deli.collect{|person| "#{katz_deli.index(person)+1}. " << person}
+    puts "The line is currently: " << current_line.join(",")
   end
 
 end
