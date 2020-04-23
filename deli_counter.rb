e25eab5651ddbf730062d52dfbde66f816277196
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-23 15:22:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,5 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
-  "There is nobody waiting to be served!"
+  puts "There is nobody waiting to be served!"
 end
