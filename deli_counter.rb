289f88cc0265dbc0f05564ddf6031028a064bb0d
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-23 14:46:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size == 0
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     katz_deli.each{|person| katz_deli.index(person) + 1 }
 
